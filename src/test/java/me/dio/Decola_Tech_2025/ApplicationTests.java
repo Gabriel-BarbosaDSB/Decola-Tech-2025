52366b9ae8cef6c03dd827138ea47c2e6d3ca499
Gabriel Barbosa <gabriel.brbs84@gmail.com>
2025-03-05 11:12:00 -0300

    Primeiro Commit

---
+++ src/test/java/me/dio/Decola_Tech_2025/ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.Decola_Tech_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
